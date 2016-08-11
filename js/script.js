fe8d8db6e173f29f8ed46856be53144a677f3353
mihalichpalich <pavlovtfspb@gmail.com>
2016-08-12 00:45:31 +0300

    Добавил интерактивную карту

--- js/script.js
+++ js/script.js
@@ -7,4 +7,42 @@ toggler.onclick = function(e){
   document.getElementById('nav').classList.toggle('nav--visible');
 };
 
+
+// Карта
+
+  var points = [
+  // Найти координаты: http://www.mapcoordinates.net/ru
+  {lat: 59.932, lng: 30.324}
+];
+
+$.ajax({
+  url: 'https://maps.googleapis.com/maps/api/js',
+    dataType: 'jsonp',
+  jsonp: 'callback',
+  data: {
+    // АХТУНГ! Это МОЙ ключ! Заведите свой!
+    key: 'AIzaSyADy7FRsZvBvHydrA49XsT48wFIB5XyrzY',
+    signed_in: true
+  },
+  success: mapInit
+});
+
+function mapInit() {
+  var map = new google.maps.Map($('#map').get(0), {
+    center: {lat: 59.932, lng: 30.348},
+    zoom: 12,
+    // disableDefaultUI: true,
+    scrollwheel: false,
+  });
+
+  var markers = [];
+
+  points.forEach(function (position, i) {
+    markers.push(new google.maps.Marker({
+      position: position,
+      map: map,
+    }));
+  });
+};
+
 });
